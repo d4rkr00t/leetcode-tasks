e57041511c0d13ffb671f3a6ce430da7d3165398
Stanislav Sysoev <d4rkr00t@gmail.com>
2020-06-03 19:09:39 +1000

    tasks: +2, total: 79

---
+++ tasks/13-roman-to-integer.py
@@ -0,0 +1,20 @@
+# Roman to Integer
+# https://leetcode.com/problems/roman-to-integer/
+# easy
+#
+# Tags: Amazon, Microsoft, Facebook
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+def romanToInt(s: str) -> int:
+    pass
+
+print(romanToInt("III"), 3)
+print(romanToInt("IV"), 4)
+print(romanToInt("IX"), 9)
+print(romanToInt("LVIII"), 58)
+print(romanToInt("MCMXCIV"), 1994)
---
+++ tasks/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,17 @@
+# Remove Duplicates from Sorted Array
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+# easy
+#
+# Tags: Facebook, Microsoft, Amazon, Google
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+def removeDuplicates(nums: [int]) -> int:
+    pass
+
+print(removeDuplicates([1,1,2]), 2)
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]), 5)
