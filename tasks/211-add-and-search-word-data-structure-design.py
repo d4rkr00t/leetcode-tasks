d7de509b0b8050329994e7ab5f310cb2536ab029
Stanislav Sysoev <d4rkr00t@gmail.com>
2020-06-16 20:01:25 +1000

    tasks: +3, total: 123

---
+++ tasks/211-add-and-search-word-data-structure-design.py
@@ -0,0 +1,37 @@
+# Add and Search Word - Data structure design
+# https://leetcode.com/problems/add-and-search-word-data-structure-design/
+# medium
+#
+# Tags: Facebook, Amazon, Google
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
---
+++ tasks/282-expression-add-operators.py
@@ -0,0 +1,20 @@
+# Expression Add Operators
+# https://leetcode.com/problems/expression-add-operators/
+# hard
+#
+# Tags: Facebook, Google
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+def addOperators(num: str, target: int) -> [str]:
+    pass
+
+print(addOperators(num = "123", target = 6), ["1+2+3", "1*2*3"])
+print(addOperators(num = "232", target = 8), ["2*3+2", "2+3*2"])
+print(addOperators(num = "105", target = 5), ["1*0+5","10-5"])
+print(addOperators(num = "00", target = 0), ["0+0", "0-0", "0*0"])
+print(addOperators(num = "3456237490", target = 9191), [])
