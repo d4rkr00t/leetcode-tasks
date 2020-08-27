709232107ed23768845e81276bb59c3107c93bef
Stanislav Sysoev <d4rkr00t@gmail.com>
2020-08-27 13:59:03 +1000

    tasks: +1, total: 147

---
+++ tasks/904-fruit-into-baskets.py
@@ -0,0 +1,23 @@
+# Fruit Into Baskets
+# https://leetcode.com/problems/fruit-into-baskets/
+# medium
+#
+# Tags: Google
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+from typing import List
+
+
+def totalFruit(tree: List[int]) -> int:
+    pass
+
+
+print(totalFruit([1, 2, 1]), 3)
+print(totalFruit([0, 1, 2, 2]), 3)
+print(totalFruit([1, 2, 3, 2, 2]), 4)
+print(totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]), 5)
