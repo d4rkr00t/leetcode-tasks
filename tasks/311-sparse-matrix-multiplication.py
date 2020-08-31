2da5fdfdbc89583e4c6f96250aeb5d6bcc9e5f5c
Stanislav Sysoev <d4rkr00t@gmail.com>
2020-08-31 14:03:52 +1000

    tasks: +2, total: 149

---
+++ tasks/311-sparse-matrix-multiplication.py
@@ -0,0 +1,21 @@
+# Sparse Matrix Multiplication
+# https://leetcode.com/problems/sparse-matrix-multiplication/
+# medium
+#
+# Tags:
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+from typing import List
+
+
+def multiply(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+    pass
+
+
+print(multiply(A=[[1, 0, 0], [-1, 0, 3]], B=[[7, 0, 0],
+                                             [0, 0, 0], [0, 0, 1]]), [[7, 0, 0], [-7, 0, 3]])
---
+++ tasks/325-maximum-size-subarray-sum-equals-k.py
@@ -0,0 +1,21 @@
+# Maximum Size Subarray Sum Equals k
+# https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/
+# medium
+#
+# Tags:
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+from typing import List
+
+
+def maxSubArrayLen(nums: List[int], k: int) -> int:
+    pass
+
+
+print(maxSubArrayLen(nums=[1, -1, 5, -2, 3], k=3), 4)
+print(maxSubArrayLen(nums=[-2, -1, 2, 1], k=1), 2)
