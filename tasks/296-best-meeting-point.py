55ce3890ac75a4124164296f86f9b9becb84e838
Stanislav Sysoev <d4rkr00t@gmail.com>
2020-09-02 19:38:40 +1000

    tasks: +2, total: 150

---
+++ tasks/296-best-meeting-point.py
@@ -0,0 +1,20 @@
+# Best Meeting Point
+# https://leetcode.com/problems/best-meeting-point/
+# hard
+#
+# Tags: Google, Amazon
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+from typing import List
+
+
+def minTotalDistance(grid: List[List[int]]) -> int:
+    pass
+
+
+print(minTotalDistance([[1, 0, 0, 0, 1], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0]]), 6)
---
+++ tasks/686-repeated-string-match.py
@@ -0,0 +1,20 @@
+# Repeated String Match
+# https://leetcode.com/problems/repeated-string-match/
+# medium
+#
+# Tags: Google, Facebook, Amazon
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+def repeatedStringMatch(a: str, b: str) -> int:
+    pass
+
+
+print(repeatedStringMatch(a="abcd", b="cdabcdab"), 3)
+print(repeatedStringMatch(a="a", b="aa"), 2)
+print(repeatedStringMatch(a="a", b="a"), 1)
+print(repeatedStringMatch(a="abc", b="wxyz"), -1)
