391a14e606291deba0605bb137c633f28dd5fda4
Stanislav Sysoev <d4rkr00t@gmail.com>
2020-10-26 13:56:48 +1100

    solves: +5, total: 15

---
+++ solutions/282-expression-add-operators.py
@@ -0,0 +1,62 @@
+# Expression Add Operators
+# https://leetcode.com/problems/expression-add-operators/
+# hard
+#
+# Tags: Facebook, Google
+#
+# Time:  O(4^n)
+# Space: O(N)
+#
+# Solution:
+# Backtracking
+
+from typing import List
+
+
+def addOperators(num: str, target: int) -> List[str]:
+    ans = []
+    N = len(num)
+
+    def backtrack(index, prev_operand, current_operand, value, string):
+        if index == N:
+            if value == target and current_operand == 0:
+                ans.append("".join(string[1:]))
+            return
+
+        current_operand = current_operand * 10 + int(num[index])
+        str_op = str(current_operand)
+
+        if current_operand > 0:
+            backtrack(index + 1, prev_operand, current_operand, value, string)
+
+        string.append("+")
+        string.append(str_op)
+        backtrack(index + 1, current_operand, 0,
+                  value + current_operand, string)
+        string.pop()
+        string.pop()
+
+        if string:
+            string.append("-")
+            string.append(str_op)
+            backtrack(index + 1, -current_operand, 0,
+                      value - current_operand, string)
+            string.pop()
+            string.pop()
+
+            string.append("*")
+            string.append(str_op)
+            backtrack(index + 1, current_operand * prev_operand, 0, value -
+                      prev_operand + (current_operand * prev_operand), string)
+            string.pop()
+            string.pop()
+
+    backtrack(0, 0, 0, 0, [])
+    return ans
+
+
+print(addOperators(num="123", target=6), ["1+2+3", "1*2*3"])
+print(addOperators(num="232", target=8), ["2*3+2", "2+3*2"])
+print(addOperators(num="105", target=5), ["1*0+5", "10-5"])
+print(addOperators(num="00", target=0), ["0+0", "0-0", "0*0"])
+print(addOperators(num="3456237490", target=9191), [])
