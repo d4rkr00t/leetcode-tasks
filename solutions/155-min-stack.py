e649860fdf99c44fed75f7d7071c3d62b46dbca5
Stanislav Sysoev <d4rkr00t@gmail.com>
2022-06-30 13:59:23 +1000

    solves: +1

---
+++ solutions/155-min-stack.py
@@ -0,0 +1,42 @@
+# Min Stack
+# https://leetcode.com/problems/min-stack/
+# easy
+#
+# Tags: Amazon, Microsoft, Google
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+
+class MinStack:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = float("inf")
+
+    def push(self, x: int) -> None:
+        self.stack.append((x, self.min))
+        self.min = min(x, self.min)
+
+    def pop(self) -> None:
+        _, prev_min = self.stack.pop()
+        self.min = prev_min
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
